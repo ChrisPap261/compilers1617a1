bfed3c39e26a003f50a3d9104635000b8a246e2b
Christos Papageorgiou <ChrisPap261@users.noreply.github.com>
2017-05-09 14:18:34 +0300

    Add files via upload

--- srt-fixer.py
+++ srt-fixer.py
@@ -4,8 +4,8 @@ import argparse
 
 
 
-restr = r'([0-9][0-9]):([0-9][0-9]):([0-9][0-9]),([0-9][0-9][0-9]) --> ([0-9][0-9]):([0-9][0-9]):([0-9][0-9]),([0-9][0-9][0-9])'
-rexp = re.compile(restr,re.IGNORECASE)
+regex = r'([0-9][0-9]):([0-9][0-9]):([0-9][0-9]),([0-9][0-9][0-9]) --> ([0-9][0-9]):([0-9][0-9]):([0-9][0-9]),([0-9][0-9][0-9])'
+rexp = re.compile(regex)
 
 parser = argparse.ArgumentParser()
 # add mandatory (positional) arguments
@@ -26,9 +26,9 @@ with open(args.fname,'r+',newline='') as ifp:
 		
 		if m is not None:
 			
-			LeftHour = int(m.group(1))
-			LeftMin = int(m.group(2)) 
-			LeftSec	= int(m.group(3))
+			LeftHour = int(m.group(1))			#	 Μεταβλητές στις οποίες αποθηκεύει 
+			LeftMin = int(m.group(2)) 			#	 τις τιμες που βρίσκει στο input
+			LeftSec	= int(m.group(3))	
 			LeftMs = int(m.group(4))
 			
 			RightHour = int(m.group(5))
@@ -38,7 +38,8 @@ with open(args.fname,'r+',newline='') as ifp:
 	
 		#------------------------------------------------------------------------
 			
-			NewLeftHour = LeftHour
+			NewLeftHour = LeftHour				#	 Μεταβλητές στις οποίες αποθηκεύει 
+			LeftMin = int(m.group(2)) 			#	 τις τιμες που θα μπουν στο output
 			NewLeftMin = LeftMin
 			NewLeftSec = LeftSec + int(args.offset)
 			NewLeftMs = LeftMs 
@@ -50,8 +51,8 @@ with open(args.fname,'r+',newline='') as ifp:
 			
 		#------------------------------------------------------------------------
 
-			ms = args.offset - int(args.offset) #mono ta deuterolepta tou offset
-			NewLeftMs = int(LeftMs + (ms*1000))
+			ms = args.offset - int(args.offset) 		#	Mόνο τo δεκαδικό μέρος του offset
+			NewLeftMs = int(LeftMs + (ms*1000))		
 			NewRightMs = int(RightMs + (ms*1000))
 
 			if NewLeftMs > 999:
@@ -62,10 +63,19 @@ with open(args.fname,'r+',newline='') as ifp:
 				NewRightSec = NewRightSec + 1
 
 		#------------------------------------------------------------------------
+						
+			NewLeftHour = str(NewLeftHour).zfill(2)  	#	Μετατροπή των μεταβλητών σε string
+			NewLeftMin = str(NewLeftMin).zfill(2)	
+			NewLeftSec = str(NewLeftSec).zfill(2)
+			NewLeftMs = str(NewLeftMs).zfill(2) 
 			
-			ps = "%d:%d:%d,%d --> %d:%d:%d,%d\n"%(NewLeftHour, NewLeftMin, NewLeftSec, NewLeftMs, NewRightHour, NewRightMin, NewRightSec, NewRightMs)
-		
-			sys.stdout.write(ps)
+			NewRightHour = str(NewRightHour).zfill(2)
+			NewRightMin = str(NewRightMin).zfill(2) 
+			NewRightSec = str(NewRightSec).zfill(2)
+			NewRightMs = str(NewRightMs).zfill(3)
+			
+			ps = "%s:%s:%s,%s --> %s:%s:%s,%s\n"%(NewLeftHour, NewLeftMin, NewLeftSec, NewLeftMs, NewRightHour, NewRightMin, NewRightSec, NewRightMs)
+			sys.stdout.write(ps)			
 		else:
 			sys.stdout.write(line)
 
